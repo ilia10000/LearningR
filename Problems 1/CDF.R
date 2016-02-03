e4514088bd6376c8b9ba697f479f836fbb4b1cd9
ilia10000 <s.ilia@rogers.com>
2016-02-03 02:37:25 -0500

    Simple cdf based calculations

--- Problems 1/CDF.R
+++ Problems 1/CDF.R
@@ -3,3 +3,8 @@ cdf = function(x)
 {
 	return (exp(-exp((1-x)/2)))
 }
+Neg = cdf(0) #P(X<0)
+Negmax10 = cdf(0)^10 #Probability that maximum of 10 trials is negative
+Posmin10 = (1-cdf(0))^10 #Probability that minimum of ten trials is positive
+
+
