0bd6dc973087ee1bc0f5f3446abc426c6fb671b4
ilia10000 <s.ilia@rogers.com>
2016-02-03 02:50:39 -0500

    Using second function for efficiencey

--- Problems 1/city.R
+++ Problems 1/city.R
@@ -1,31 +1,4 @@
 #Some interesting logistics examples
-#Function 1
-Quint = function(n)
-{
-cut_off_burgs = NULL
-for (i in 1:n)
-{
-burgs = 0 
-X=rbinom(8,1,0.65);
-#Is burg A cut off?
-if (sum(X[1:3])==3) burgs = burgs + 1
-#Is burg B cut off?
-if
-(sum(X[3:5])==3) burgs = burgs + 1
-#Is burg C cut off?
-if (sum(X[5:7])==3) burgs = burgs + 1
-#Is burg D cut off?
-if (sum(X[c(1,7:8)])==3) burgs = burgs + 1
-#Is burg E cut off?
-if (sum(X[c(2,4,6,8)])==4) burgs = burgs + 1
-cut_off_burgs = c(cut_off_burgs,burgs)
-}
-return(cut_off_burgs)
-}
-set.seed(1)
-Quint(10)
-
-#Function 2
 Quint = function(n){
 closures=NULL
 #sampling n times
