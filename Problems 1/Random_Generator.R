1a8fb26abcc340af307da8ffd578c2b58cf6ceec
ilia10000 <s.ilia@rogers.com>
2016-02-03 01:36:15 -0500

    Wrapper function
    Put distributions into wrapper function

--- Problems 1/Random_Generator.R
+++ Problems 1/Random_Generator.R
@@ -1,6 +1,25 @@
 #Let's use rchisq to do some random sampling
-X_arr = rchisq(100, 1) #GAM(2,1/2)
-X_arr = rchisq(100,1) + rchisq(100,1) + rchisq(100,1) #Chi-Squared(df=3)
-X_arr = rchisq(100,1) + rchisq(100,1) #EXP(1/2)
-X_arr = sqrt(rchisq(100,1)) #N(0,1)
-X_arr = 4 + 3*sqrt(rchisq(100,1)) #N(4,9)
+RG = function(N)
+{
+	if (N == 0){
+		X_arr = rchisq(100, 1) #GAM(2,1/2)
+		return (X_arr)
+	}
+	if (N == 1){
+		X_arr = rchisq(100,1) + rchisq(100,1) + rchisq(100,1) #Chi-Squared(df=3)
+		return (X_arr)
+	}
+	if (N == 2){
+		X_arr = rchisq(100,1) + rchisq(100,1) #EXP(1/2)
+		return (X_arr)
+	}
+	if (N == 3){
+		X_arr = sqrt(rchisq(100,1)) #N(0,1)
+		return (X_arr)
+	}
+	if (N == 4){
+		X_arr = 4 + 3*sqrt(rchisq(100,1)) #N(4,9)
+		return (X_arr)
+	}
+	return (c(1:100))
+}
