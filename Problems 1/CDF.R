10928c4ca9a8cc7ef1a81fe326897d3a5155f286
ilia10000 <s.ilia@rogers.com>
2016-02-03 02:37:52 -0500

    Slightly more complex cdf calculations

--- Problems 1/CDF.R
+++ Problems 1/CDF.R
@@ -6,5 +6,8 @@ cdf = function(x)
 Neg = cdf(0) #P(X<0)
 Negmax10 = cdf(0)^10 #Probability that maximum of 10 trials is negative
 Posmin10 = (1-cdf(0))^10 #Probability that minimum of ten trials is positive
-
+Posmin3by10of5 = (Posmin10^3)*((1-Posmin10)^2)*choose(5,3)#P that 5 sets of 10 trials have 3 with positive min
+Posmin4by10of5 = (Posmin10^4)*((1-Posmin10))*choose(5,4)#P that 5 sets of 10 trials have 4 with positive min
+Posmin5by10of5 = (Posmin10^5)#P that 5 sets of 10 trials have 5 with positive min
+Posmin3ormore = Posmin3by10of5 + Posmin4by10of5 + Posmin5by10of5 
 
